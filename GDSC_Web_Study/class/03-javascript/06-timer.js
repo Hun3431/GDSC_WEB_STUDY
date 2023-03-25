24d41e616965fa9e977bdbd5b0b6011973af2410
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-03-25 20:58:36 +0900

    Fix remove timer duplication

--- GDSC_Web_Study/class/03-javascript/06-timer.js
+++ GDSC_Web_Study/class/03-javascript/06-timer.js
@@ -1,27 +1,34 @@
 var num
+var isStarted = false;
 
 const auth = () => {
-    num = String(Math.floor(Math.random()*1000000)).padStart(6, '0')
-    console.log(num)
-    let time = 180
-    alert('인증번호 발급 완료')
-    const interval = setInterval(() => {
+    if(!isStarted) {
+        isStarted = true
+        document.getElementById('joinbtn').disabled = false
+        num = String(Math.floor(Math.random()*1000000)).padStart(6, '0')
+        console.log(num)
+        let time = 180
+        alert('인증번호 발급 완료')
+        const interval = setInterval(() => {
         if(time >= 0) {
             time--
             document.getElementById('time').innerText = Math.floor(time/60) +':'+String(time%60).padStart(2, '0')
         }
         else {
+            isStarted = false
             document.getElementById('joinbtn').disabled = true
             document.getElementsById('time').innerText = '만료'
             clearInterval(interval)
         }
-    }, 1000)
+        }, 1000)
+    }
+    else {
+        alert('이미 인증번호를 발급 받았습니다.')
+    }
 }
 
 const join = () => {
     const usernum = Number(document.getElementById('countinput').value)
-    console.log('num : ' + num)
-    console.log('usernum : ' + usernum)
 
     if(num == usernum) {
         alert('정상적으로 가입이 완료되었습니다.')
