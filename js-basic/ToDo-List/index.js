0d4c7a1e0a0d711d8323273ef52f40ed622da8d7
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-07-20 01:55:13 +0900

    Feat js-basic todo-list EventListener(btn)

--- js-basic/ToDo-List/index.js
+++ js-basic/ToDo-List/index.js
@@ -1,14 +1,23 @@
 const todoList = document.getElementById("todo-list");
 const inputValue = document.getElementById("todo-input");
 
+const createTodo = () => {
+  const newLi = document.createElement("li");
+  const newSpan = document.createElement("span");
+  const newBtn = document.createElement("button");
+
+  newBtn.addEventListener("click", () => {
+    newLi.classList.toggle("complete");
+  });
+  newSpan.textContent = inputValue.value;
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+  todoList.appendChild(newLi);
+  inputValue.value = "";
+};
+
 const keyCodeCheck = () => {
   if (window.event.keyCode === 13 && inputValue.value != "") {
-    const newLi = document.createElement("li");
-    const newSpan = document.createElement("span");
-
-    newSpan.textContent = inputValue.value;
-    newLi.appendChild(newSpan);
-    todoList.appendChild(newLi);
-    inputValue.value = "";
+    createTodo();
   }
 };
